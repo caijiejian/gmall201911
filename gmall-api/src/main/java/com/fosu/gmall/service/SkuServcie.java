00357aedba68d6e30b3d25c1461f38e1d6946d40
蔡杰鉴 <1150074271@qq.com>
2019-12-12 17:25:45 +0800

    test reset mixes

--- gmall-api/src/main/java/com/fosu/gmall/service/SkuServcie.java
+++ gmall-api/src/main/java/com/fosu/gmall/service/SkuServcie.java
@@ -2,7 +2,13 @@ package com.fosu.gmall.service;
 
 import com.fosu.gmall.bean.PmsSkuInfo;
 
+import java.util.List;
+
 public interface SkuServcie {
     void saveSkuInfo(PmsSkuInfo pmsSkuInfo);
     PmsSkuInfo getSkuInfo(String skuId);
+
+    List<PmsSkuInfo> getSkuInfos(String productId);
+
+    List<PmsSkuInfo> getSkuSaleAttrValueListBySpu(String productId);
 }
